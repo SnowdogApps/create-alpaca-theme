88b6e78974888b098862f8db99b9044e1bd2886b
Patryk <patryk.bura@snow.dog>
2022-01-04 11:08:16 +0100

    formatting

--- src/local-env-actions.mjs
+++ src/local-env-actions.mjs
@@ -4,54 +4,54 @@ export function replaceJSONContents(path, json) {
   fs.writeFileSync(path, JSON.stringify(json), err => {
     if (err) {
       throw (`Error while copying template files\n${err}`)
-    };
-  });
-};
+    }
+  })
+}
 
 export function renameTheme(path, themeName) {
   fs.readFile(path, 'utf-8', (err, data) => {
     if (err) {
       throw err
-    };
+    }
 
     const newValue = data.replace(/YOUR_THEME_NAME/gim, themeName);
 
     fs.writeFile(path, newValue, 'utf-8', err => {
       if (err) {
         throw err
-      };
-    });
-  });
-};
+      }
+    })
+  })
+}
 
 export function renameBrowserSyncPaths(path, themeName) {
   fs.readFile(path, 'utf-8', (err, data) => {
     if (err) {
       throw err
-    };
+    }
 
     const newValue = data.replace(/alpaca-boilerplate.test/gim, `${themeName}.test`);
 
     fs.writeFile(path, newValue, 'utf-8', err => {
       if (err) {
         throw err
-      };
-    });
-  });
-};
+      }
+    })
+  })
+}
 
 export function createDirectory(path) {
   fs.promises.mkdir(path, { recursive: true }, err => {
     if (err) {
       throw err
-    };
-  });
-};
+    }
+  })
+}
 
 export function createThmeRegistrationFiles(path, payload) {
   fs.writeFile(path, payload, err => {
     if (err) {
       throw err
-    };
-  });
-};
+    }
+  })
+}
