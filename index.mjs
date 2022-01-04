2fdaf3270a621faeed2944402d070311ff354499
Patryk <patryk.bura@snow.dog>
2022-01-04 11:08:49 +0100

    code quality improvments

--- index.mjs
+++ index.mjs
@@ -2,8 +2,11 @@
 
 import inquirer from 'inquirer';
 import cliProgress from 'cli-progress';
-import _colors from 'colors'
+import colors from 'colors'
+import Spinner from './utils/spinner.mjs'
 import { readFile } from 'fs/promises';
+import { composerRequire } from './src/composer-actions.mjs';
+import { magentoUpgrade } from './src/magento-actions.mjs'
 import {
   validateInput,
   validateComposer,
@@ -20,30 +23,33 @@ import {
   installFrontools,
   compileFiles
 } from './src/frontools-actions.mjs'
-import { composerRequire } from './src/composer-actions.mjs';
-import { magentoUpgrade } from './src/magento-actions.mjs'
+import {
+  NOT_MAGENTO_MSG_BOTTOM,
+  NOT_MAGENTO_MSG_TOP,
+  CHECK_MARK_CHARACTER,
+  PACKAGE_PATH,
+  LOADING_BAR,
+  TEMPLATE_PATHS,
+  LOCAL_ENV_PATHS
+} from './utils/constants.mjs';
 
-const alpacaPackagesPath = 'snowdog/module-alpaca-packages'
-const frontoolsPath = 'snowdog/frontools'
-const barInfoColor = _colors.yellow
-const blue = _colors.blue
-const red = _colors.red
 const log = console.log
-const spinnerPath = ["\\", "|", "/", "-"];
-const spinnerSpeed = 100;
+const barInfoColor = colors.yellow
 const progressBar = new cliProgress.SingleBar({
-  format: '  |' + _colors.cyan('{bar}') + '| {percentage}% || {info}',
-  barCompleteChar: '\u2588',
-  barIncompleteChar: '\u2591',
-  hideCursor: true
+  format: LOADING_BAR.FORMAT,
+  barCompleteChar: LOADING_BAR.COMPLETE_CHAR,
+  barIncompleteChar: LOADING_BAR.INCOMPLETE_CHAR,
+  hideCursor: LOADING_BAR.CURSOR_HIDDEN,
+  barsize: LOADING_BAR.SIZE
 });
+const spinner = new Spinner()
 
 /*
-  Check if valid Magento instance. If not throw error.
+  Check if valid Magento instance. If not log error.
 */
 if (isMagentoInstance()) {
   console.clear()
-  log(blue('Snowdog Alpaca Theme CLI v1.0.0\n'))
+  log(colors.blue('Snowdog Alpaca Theme CLI v1.0.0\n'))
 
   inquirer.prompt([
     {
@@ -55,13 +61,8 @@ if (isMagentoInstance()) {
   ])
   .then(async answers => {
     try {
-      let iteratorValue = 0; // Spinner character iterator value
-      const spinner = setInterval(() => {
-        process.stdout.write("\r" + spinnerPath[iteratorValue++]);
-        iteratorValue &= 3;
-      }, spinnerSpeed);
-
-      console.time(blue('Finished in')) // Start task time counter
+      console.time(colors.blue('Finished in')) // Start task time counter
+      spinner.start()
 
       progressBar.start(100, 0, {
         info: barInfoColor("Validating composer..."),
@@ -71,12 +72,12 @@ if (isMagentoInstance()) {
       progressBar.update(2, {
         info: barInfoColor("Downloading Alpaca Packages...")
       });
-      await composerRequire(alpacaPackagesPath)
+      await composerRequire(PACKAGE_PATH.ALPACA_PACKAGES)
 
       progressBar.update(30, {
         info: barInfoColor("Downloading Frontools...")
       });
-      await composerRequire(frontoolsPath)
+      await composerRequire(PACKAGE_PATH.FRONTOOLS)
 
       progressBar.update(45, {
         info: barInfoColor("Installing Frontools...")
@@ -86,24 +87,24 @@ if (isMagentoInstance()) {
       progressBar.update(55, {
         info: barInfoColor("Configuring themes.json...")
       });
-      const themesJson = JSON.parse(await readFile(new URL('./templates/themes.json.sample', import.meta.url)));
-      replaceJSONContents('dev/tools/frontools/config/themes.json', themesJson)
+      const themesJson = JSON.parse(await readFile(new URL(TEMPLATE_PATHS.THEMES_JSON, import.meta.url)));
+      replaceJSONContents(LOCAL_ENV_PATHS.THEMES_JSON, themesJson)
 
       progressBar.update(57, {
         info: barInfoColor("Replace theme name in themes.json with your theme name")
       });
-      renameTheme('dev/tools/frontools/config/themes.json', answers.name)
+      renameTheme(LOCAL_ENV_PATHS.THEMES_JSON, answers.name)
 
       progressBar.update(59, {
         info: barInfoColor("Configuring browser-sync.json...")
       });
-      const browserSyncJson = JSON.parse(await readFile(new URL('./templates/browser-sync.json.sample', import.meta.url)));
-      replaceJSONContents('dev/tools/frontools/config/browser-sync.json', browserSyncJson)
+      const browserSyncJson = JSON.parse(await readFile(new URL(TEMPLATE_PATHS.BROWSER_SYNC, import.meta.url)));
+      replaceJSONContents(LOCAL_ENV_PATHS.BROWSER_SYNC, browserSyncJson)
 
       progressBar.update(61, {
         info: barInfoColor("Replace theme name in browser-sync.json with your theme name")
       });
-      renameBrowserSyncPaths('dev/tools/frontools/config/browser-sync.json', answers.name)
+      renameBrowserSyncPaths(LOCAL_ENV_PATHS.BROWSER_SYNC, answers.name)
 
       progressBar.update(63, {
         info: barInfoColor("Creating child theme directory...")
@@ -113,14 +114,14 @@ if (isMagentoInstance()) {
       progressBar.update(65, {
         info: barInfoColor("Creating theme.xml file...")
       });
-      const themeXML = await readFile(new URL('./templates/theme.xml.sample', import.meta.url));
+      const themeXML = await readFile(new URL(TEMPLATE_PATHS.THEME_XML, import.meta.url));
       const themeXMLUpdated = themeXML.toString().replace(/YOUR_THEME_NAME/gim, answers.name)
       createThmeRegistrationFiles(`app/design/frontend/Snowdog/${answers.name}/theme.xml`, themeXMLUpdated)
 
       progressBar.update(67, {
         info: barInfoColor("Creating registration.php file...")
       });
-      const registrationPhp = await readFile(new URL('./templates/registration.php.sample', import.meta.url));
+      const registrationPhp = await readFile(new URL(TEMPLATE_PATHS.REGISTRATION, import.meta.url));
       const registrationPhpUpdated = registrationPhp.toString().replace(/YOUR_THEME_NAME/gim, answers.name)
       createThmeRegistrationFiles(`app/design/frontend/Snowdog/${answers.name}/registration.php`, registrationPhpUpdated)
 
@@ -135,27 +136,31 @@ if (isMagentoInstance()) {
       await compileFiles()
 
       progressBar.update(100, {
-        info: blue("Enjoy Alpaca :)")
+        info: colors.blue("Enjoy Alpaca :)")
       });
 
-      clearInterval(spinner)
-      process.stdout.write("\r" + '\u2713') // Unicode check mark character
+      process.stdout.write("\r" + CHECK_MARK_CHARACTER)
+      spinner.stop()
       progressBar.stop();
-      console.timeEnd(blue('Finished in')) // Stop task time counter
+      console.timeEnd(colors.blue('Finished in')) // Stop task time counter
 
-      log(blue('\nInstallation finished succefuly!'))
-      log(blue('Go to Admin Panel -> Content -> Design -> Configuration and choose your theme'), `(${barInfoColor(answers.name)}).`)
-      log(blue('\nVisit Alpaca Docs to learn how to work with Alpaca Theme.'))
-      log(blue('To see exemplary code go to Alpaca Boilerplate.\n'))
+      log(colors.blue('\nInstallation finished succefuly!'))
+      log(colors.blue('Go to Admin Panel -> Content -> Design -> Configuration and choose your theme'), `(${barInfoColor(answers.name)}).`)
+      log(colors.blue('\nVisit Alpaca Docs to learn how to work with Alpaca Theme.'))
+      log(colors.blue('To see exemplary code go to Alpaca Boilerplate.\n'))
       log(barInfoColor('2022 Snowdog || https://snow.dog || https://github.com/SnowdogApps \n'))
     }
-    catch (exception_var) {
+    catch (error) {
       progressBar.update(0, {
-        info: red("Installation failed.")
+        info: colors.red("Installation failed.")
       });
+      spinner.stop()
       progressBar.stop();
-      log(`\n${red(exception_var)}`)
+      log(`\n${colors.red(error)}`)
       process.exit()
     }
   })
+} else {
+  console.error(colors.colors.red(NOT_MAGENTO_MSG_TOP))
+  console.error(colors.yellow(NOT_MAGENTO_MSG_BOTTOM))
 }
