bfaa48f5e63f0bfca55e9e29cf3c7650e059962e
Patryk <patryk.bura@snow.dog>
2022-01-08 13:02:30 +0100

    improve name validators

--- src/validators.js
+++ src/validators.js
@@ -5,39 +5,37 @@ import {
   NAME_TO_SHORT_MSG,
   NAME_NOT_PROVIDED_MSG,
   REGISTRATION_NAME_NOT_SINGULAR,
-  BANNED_NAMES_MSG
+  BANNED_NAMES_MSG,
+  BANNED_NAMES_LIST
 } from '../utils/constants.js'
 
-export function validateName(inputString) {
-  if (inputString.length === 0) {
-    return NAME_NOT_PROVIDED_MSG
-  }
-  if (inputString.length < NAME_MINIMUM_LENGTH) {
-    return NAME_TO_SHORT_MSG
-  }
-  if (inputString === 'Alpaca Theme') {
-    return BANNED_NAMES_MSG
-  }
+function getNameValidator(customRules = []) {
+  return (name) => {
+    if (name.length === 0) {
+      return NAME_NOT_PROVIDED_MSG
+    }
+    if (name.length < NAME_MINIMUM_LENGTH) {
+      return NAME_TO_SHORT_MSG
+    }
+    if (BANNED_NAMES_LIST.includes(name)) {
+      return BANNED_NAMES_MSG
+    }
 
-  return true
+    const customRulesResults = customRules.map((rule) => rule(name))
+
+    return customRulesResults.length ? customRulesResults[0] : true
+  }
 }
 
-export function validateRegistrationName(inputString) {
-  if (inputString.length === 0) {
-    return NAME_NOT_PROVIDED_MSG
-  }
-  if (inputString.length < NAME_MINIMUM_LENGTH) {
-    return NAME_TO_SHORT_MSG
-  }
-  if (inputString.split(' ').length > 1) {
+export const validateName = getNameValidator() // TODO function?
+
+export const validateRegistrationName = getNameValidator([(name) => {
+  if (name.split(' ').length > 1) {
     return REGISTRATION_NAME_NOT_SINGULAR
   }
-  if (inputString === 'alpaca') {
-    return BANNED_NAMES_MSG
-  }
 
   return true
-}
+}])
 
 export function validateComposer() {
   return promiseExec('composer -v', (msg) => {
