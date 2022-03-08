2654f534ca21b8d2c44ce6bc8e7ace29219062a5
Patryk <patryk.bura@snow.dog>
2022-03-08 19:32:57 +0100

    Use user database credentials

--- src/database-actions.js
+++ src/database-actions.js
@@ -4,12 +4,7 @@ import { readFile } from 'fs/promises'
 
 export default async function runQueries() {
   const dataSql = await readFile(new URL('../templates/database/queries.sql', import.meta.url))
-  const connection = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "root",
-    database: "testbase"
-  })
+  const connection = mysql.createConnection(await getDatabaseDetails())
 
   const errors = await execute(connection, dataSql)
   connection.end()
@@ -51,4 +46,31 @@ async function execute(connection, dataSql) {
       }).filter((x) => x))
     })
   })
+}
+
+// GETTING DATABASE VALUES FROM .ENV TABLE IN PHP FORMAT
+async function getDatabaseDetails() {
+
+  const file = await readFile(('./app/etc/env.php'))
+  const dbTable = file.toString().split('[').filter(x => x.includes('host'))
+  const host = getValue(dbTable, 'host')
+  const database = getValue(dbTable, 'dbname')
+  const user = getValue(dbTable, 'username')
+  const password = getValue(dbTable, 'password')
+
+  return {
+    host,
+    user,
+    password,
+    database
+  }
+}
+
+function getValue(table, name) {
+  return table.join('')
+    .split(',')
+    .filter((x) => x.includes(name))[0]
+    .split(' ')
+    .pop()
+    .replace(/['"]+/g, '')
 }
