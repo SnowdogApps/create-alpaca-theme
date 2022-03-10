bc3cc3023eadd24c1789f6c43916076bd5641a22
Patryk <patryk.bura@snow.dog>
2022-03-10 13:59:39 +0100

    Edit succes message

--- utils/messages.js
+++ utils/messages.js
@@ -8,8 +8,7 @@ export const CLISuccesMessage = (themeName) => {
     colors.blue('\n\nGo to Admin Panel -> Content -> Design -> Configuration and choose your theme'),
     (`(${colors.yellow(themeName)}).`),
     colors.blue(`\nUse ${colors.yellow('\'yarn dev\'')} in Snowdog_Components dir to run components in Fractal`),
-    colors.blue('\nVisit Alpaca Docs to learn how to work with Alpaca Theme.'),
-    colors.blue('\nTo see exemplary code go to Alpaca Boilerplate.'),
+    colors.blue('\nRead Alpaca Docs to learn how to work with Alpaca Theme.'),
     colors.yellow('\n\n2022 Snowdog || https://snow.dog || https://github.com/SnowdogApps \n')
   )
 }
