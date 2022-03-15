82f5e9e5e61e78e9d5ea0a4df7b5f88a87be1142
Patryk <patryk.bura@snow.dog>
2022-03-15 14:18:08 +0100

    formatting

--- constants/directioriesList.js
+++ constants/directioriesList.js
@@ -1,14 +1,16 @@
+// Directories to be created before files setup
+
 export const directoriesList = [
   '/',
   '/styles',
+  '/Magento_Checkout/styles',
   '/Snowdog_Components/docs/styles',
-  '/Snowdog_Components/components/Atoms/variables',
-  '/Magento_Checkout/styles'
+  '/Snowdog_Components/components/Atoms/variables'
 ]
 
 export const exemplaryComponentDirectories = [
-  '/Snowdog_Components/components/Molecules/button/',
-  '/Snowdog_Components/components/styles/'
+  '/Snowdog_Components/components/styles/',
+  '/Snowdog_Components/components/Molecules/button/'
 ]
 
 export const mediaDirList = [
