bce0ad42f2ba0ab005dc2e495bb6d97c1cd674e2
Patryk <patryk.bura@snow.dog>
2022-03-30 11:36:12 +0200

    #10 - Add import of email styles

--- src/local-env-actions.js
+++ src/local-env-actions.js
@@ -124,7 +124,7 @@ export async function addBaseStyles(themeName) {
   await prependImport(chechoutPath, checkoutText, themeName, null, 'variables', 'YOUR_THEME_NAME')
 
   // CREATING THEME LEVEL STYLES
-  await addFilesFromDir(ENV_PATH.ALPACA_STYLES_DIR, themeName, 'email', '/styles')
+  await addFilesFromDir(ENV_PATH.ALPACA_STYLES_DIR, themeName, '_', '/styles')
 
   const themeLevelStyles = await listFiles(themeLevelStylesPath)
 
